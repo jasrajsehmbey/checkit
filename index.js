e7e83df14497dac1142e24b72e73be938eca37f8
jasrajsehmbey <jasrajsehmbey@gmail.com>
2023-04-07 15:11:57 +0530

    backend done

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ db.connect(err=>{
 
 const app = express();
 
+app.use(express.json());
+
 //create database
 app.get("/createdb",(req,res)=>{
     let sql = "CREATE DATABASE nodemysql";
@@ -45,7 +47,42 @@ app.get('/:userid',(req,res)=>{
 });
 
 //insert into table
+app.post('/insert/:userid',(req,res)=>{
+    const data = req.body;
+    var id = req.params.userid;
+    
+    db.query(`INSERT INTO ${id} SET ?`,data,(err,result)=>{
+        if(err){
+            throw err;
+        }
+        res.send(result);
+    });
+});
 
+app.delete("/delete/:userid", (req, res) => {
+    var id = req.params.userid;
+    var note = req.body.note;
+    const sql = `DELETE FROM ${id} WHERE note = "${note}"`;
+    db.query(sql, (err) => {
+      if (err) {
+        throw err;
+      }
+      const temp = `${note} from ${id} table deleted`;
+      res.send(temp);
+    });
+  });
+  
+  // get data
+  app.get("/get/:userid", (req, res) => {
+    var id = req.params.userid;
+    const sql = `SELECT * FROM ${id}`;
+    db.query(sql, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.send(result);
+    });
+  });
 
 app.listen('3000',()=>{
     console.log('Server Started on port 3000')
