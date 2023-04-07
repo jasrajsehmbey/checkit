628676712710d27dcb755c2d2f1687e7972c70b7
jasrajsehmbey <jasrajsehmbey@gmail.com>
2023-04-07 14:32:05 +0530

    second:

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const db = mysql.createConnection({
     host : 'localhost',
     user : 'root',
     password: 'system',
+    database: 'nodemysql',
 });
 
 //connect to mysql
@@ -29,6 +30,22 @@ app.get("/createdb",(req,res)=>{
         res.send("database created");
     });
 });
+
+//create table
+app.get('/:userid',(req,res)=>{
+    var id = req.params.userid;
+    const sql = `CREATE TABLE ${id} (id int AUTO_INCREMENT PRIMARY KEY, note VARCHAR(255))`;
+    db.query(sql,err=>{
+        if(err){
+            throw err;
+        }
+        const temp = `${id} table created`;
+        res.send(temp);
+    });
+});
+
+//insert into table
+
 
 app.listen('3000',()=>{
     console.log('Server Started on port 3000')
